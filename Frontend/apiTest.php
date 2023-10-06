c96ff536079e869831b318734abf243380c34c98
Kalana Coder <91042617+kalanadidulanga@users.noreply.github.com>
2023-10-06 08:11:29 +0530

    aaaa

--- Frontend/apiTest.php
+++ Frontend/apiTest.php
@@ -2,6 +2,7 @@
 
 require_once 'vendor/autoload.php';
 use Pj8912\PhpBardApi\Bard;
+
 //two keys are required which are two cookies values
 $_ENV['BARD_API_KEY_X'] = "bwjHTq4ALLZFrfdHKkTBKXkmjgTJSPANeiTwMrpPriYZwDmLXxk051yTpVwK8puu_eN7GQ.";
 $_ENV['BARD_API_KEY_Y'] = "sidts-CjIB3e41hUJnXeBQNNpEegB3cbBwjSCDnqgPfjHuQ6_20YNR2aEgPKda1V65Zk0fp_o42BAA";
